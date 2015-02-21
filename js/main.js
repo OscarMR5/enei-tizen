3114d1a3f02ff505d6114a25031e79fc56ac927a
Pablo Guijarro Ramirez <sky10pg@gmail.com>
2015-02-21 15:53:26 +0100

    Añadido hola.js para Oauth

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
+
+var contador=0;
+
 $(window).load(function(){
 	
-		var contador=0;
-		$("#btnPrueba").on("click",function(){
-			
-			contador++;
-			$("#txtTexto").text("Ha pulsado"+contador+" veces");
-			
-		});
+	   
+	    $("#content").load("js/views/hola.html",btnPrueba);
+	
+		
+		
 		
 	});
+
+function btnPrueba(){
+	$("#btnPrueba").on("click",function(){
+		
+		contador++;
+		$("#txtTexto").text("Ha pulsado"+contador+" veces");
+		
+	});
+}
